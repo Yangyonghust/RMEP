593f85019fe3e79f9d7a72a27fdf8aacbe36dba2
Yangyonghust <1624445843@qq.com>
2020-04-18 17:59:02 +0800

    MEP model by R

---
+++ R/set.R
@@ -0,0 +1,3 @@
+############## library ###################
+library(pracma)    #Solving Nonlinear Equations in MEP calculations
+library(humidity)  #calculating  specific humidity for MEP modele input variable
