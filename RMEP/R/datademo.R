13e3bae92197c74b7d0fd445c96d81d77f94306b
Yangyong <1624445843@qq.com>
2021-03-22 21:02:02 +0800

    update

---
+++ RMEP/R/datademo.R
@@ -0,0 +1,23 @@
+#' Variables for MEP model analysis in Vignettes.
+#'
+#' A dataset containing the time series with variables of almost 1170.
+#' datademo.
+#'
+#' @format A data frame with 1170 rows and 13 variables:
+#' \describe{
+#'   \item{year}{year of 2016}
+#'   \item{month}{June}
+#'   \item{day}{1st to 30th}
+#'   \item{hour}{0 to 23, in hour}
+#'   \item{minute}{0 and 30, in minute}
+#'   \item{H}{observed sensible heat, in W/m2}
+#'   \item{LE}{observed latent heat, in W/m2}
+#'   \item{TA}{air temperature, in deg C}
+#'   \item{TS}{surface temperature, in deg C}
+#'   \item{NETRAD}{net radiation, in W/m2}
+#'   \item{RH}{Relative humidity, in percent}
+#'   \item{G}{observed ground heat, in W/m2}
+#'   \item{PA}{atmospheric pressure, in Kpa}
+#'   ...
+#' }
+"datademo"
